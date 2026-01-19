472e36de7f3a53136daed16e99db73c008b0b4bb
Maryse M. <Maryse.Mongeau@cegepoutaouais.qc.ca>
2026-01-19 15:04:24 -0500

    Cours1

---
+++ Cours1.py
@@ -0,0 +1,22 @@
+print("Bonjour la classe!")
+print(3 + 6)
+a = 3
+b = 5
+print(a + b)
+c = a + b
+print(c)
+print(a * b) # multiplication
+print(a / b) # division
+print(a - b)
+print(9 % 2) # modulo : reste de la division
+print(9 // 2) # division entière
+print(b ** 3) # exposant
+print(a > b)
+print(a < b)
+print(a >= b) # plus grand ou égal
+print(a <= b) # plus petit ou égal
+print(a == b) # vérifier s'ils sont égaux
+print(a != b) # différents
+print(1 > 3 or 4 > 3) # or : OU
+print(1 > 3 and 4 > 3) # and : ET
+print(not 3 > 4) # not : n'est pas
---
+++ Ex1.1.py
@@ -0,0 +1,8 @@
+Bande_passante_totale = 1000
+Bande_passante_utilisee = 673
+
+Bande_passante_disponible = Bande_passante_totale - Bande_passante_utilisee
+Pourcentage_utilisation = Bande_passante_utilisee * 100 / Bande_passante_totale
+
+print("Bande passante disponible", Bande_passante_disponible )
+print("Pourcentage d'utilisation", Pourcentage_utilisation )
