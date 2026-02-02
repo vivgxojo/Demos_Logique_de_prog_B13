ed7b83cccbe4345ba7bef9de074eb739bd52bbd0
Maryse M. <Maryse.Mongeau@cegepoutaouais.qc.ca>
2026-02-02 14:41:00 -0500

    Démo Ex3.1 Catégories de ports

---
+++ Ex3.1.py
@@ -0,0 +1,17 @@
+# Catégories de ports
+# Demander le no de port
+port = int(input("Entrez le numéro de port : "))
+
+# Déterminer la catégorie
+categorie = None    # Initialiser la variable pour qu'elle existe
+if 0 <= port <= 1023:
+    categorie = "bien connus"
+elif 1024 <= port <= 49151:
+    categorie = "enregistrés"
+elif 49152 <= port <= 65535:
+    categorie = "dynamiques/privés"
+else:
+    print("Erreur : Port non valide")
+
+# Afficher la catégorie
+print("La catégore est port", categorie)
